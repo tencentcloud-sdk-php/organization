92256c8913846e985384c2cb527706d833a024e9
tencentcloudapi <tencentcloudapi@tencent.com>
2023-09-22 08:38:49 +0800

    release 3.0.983

--- src/TencentCloud/Organization/V20210331/Models/AddOrganizationMemberEmailResponse.php
+++ src/TencentCloud/Organization/V20210331/Models/AddOrganizationMemberEmailResponse.php
@@ -20,17 +20,29 @@ use TencentCloud\Common\AbstractModel;
 /**
  * AddOrganizationMemberEmail返回参数结构体
  *
+ * @method integer getBindId() 获取绑定Id
+注意：此字段可能返回 null，表示取不到有效值。
+ * @method void setBindId(integer $BindId) 设置绑定Id
+注意：此字段可能返回 null，表示取不到有效值。
  * @method string getRequestId() 获取唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
  * @method void setRequestId(string $RequestId) 设置唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
  */
 class AddOrganizationMemberEmailResponse extends AbstractModel
 {
     /**
+     * @var integer 绑定Id
+注意：此字段可能返回 null，表示取不到有效值。
+     */
+    public $BindId;
+
+    /**
      * @var string 唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
      */
     public $RequestId;
 
     /**
+     * @param integer $BindId 绑定Id
+注意：此字段可能返回 null，表示取不到有效值。
      * @param string $RequestId 唯一请求 ID，每次请求都会返回。定位问题时需要提供该次请求的 RequestId。
      */
     function __construct()
@@ -46,6 +58,10 @@ class AddOrganizationMemberEmailResponse extends AbstractModel
         if ($param === null) {
             return;
         }
+        if (array_key_exists("BindId",$param) and $param["BindId"] !== null) {
+            $this->BindId = $param["BindId"];
+        }
+
         if (array_key_exists("RequestId",$param) and $param["RequestId"] !== null) {
             $this->RequestId = $param["RequestId"];
         }
