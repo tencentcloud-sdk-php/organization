498127d6a0550cd5b2493f32cfd371fe489922bf
tencentcloudapi <tencentcloudapi@tencent.com>
2024-09-23 15:06:07 +0800

    release 3.0.1231

--- src/TencentCloud/Organization/V20210331/Models/InviteOrganizationMemberRequest.php
+++ src/TencentCloud/Organization/V20210331/Models/InviteOrganizationMemberRequest.php
@@ -18,8 +18,10 @@ namespace TencentCloud\Organization\V20210331\Models;
 use TencentCloud\Common\AbstractModel;
 
 /**
- * CreateOrganizationMember请求参数结构体
+ * InviteOrganizationMember请求参数结构体
  *
+ * @method integer getMemberUin() 获取被邀请账号Uin。
+ * @method void setMemberUin(integer $MemberUin) 设置被邀请账号Uin。
  * @method string getName() 获取成员名称。最大长度为25个字符，支持英文字母、数字、汉字、符号+@、&._[]-:,
  * @method void setName(string $Name) 设置成员名称。最大长度为25个字符，支持英文字母、数字、汉字、符号+@、&._[]-:,
  * @method string getPolicyType() 获取关系策略。取值：Financial
@@ -28,23 +30,26 @@ use TencentCloud\Common\AbstractModel;
  * @method void setPermissionIds(array $PermissionIds) 设置成员财务权限ID列表。取值：1-查看账单、2-查看余额、3-资金划拨、4-合并出账、5-开票、6-优惠继承、7-代付费，1、2 默认必须
  * @method integer getNodeId() 获取成员所属部门的节点ID。可以通过[DescribeOrganizationNodes](https://cloud.tencent.com/document/product/850/82926)获取
  * @method void setNodeId(integer $NodeId) 设置成员所属部门的节点ID。可以通过[DescribeOrganizationNodes](https://cloud.tencent.com/document/product/850/82926)获取
- * @method string getAccountName() 获取账号名称。最大长度为25个字符，支持英文字母、数字、汉字、符号+@、&._[]-:,
- * @method void setAccountName(string $AccountName) 设置账号名称。最大长度为25个字符，支持英文字母、数字、汉字、符号+@、&._[]-:,
  * @method string getRemark() 获取备注。
  * @method void setRemark(string $Remark) 设置备注。
- * @method integer getRecordId() 获取成员创建记录ID。创建异常重试时需要
- * @method void setRecordId(integer $RecordId) 设置成员创建记录ID。创建异常重试时需要
+ * @method string getIsAllowQuit() 获取是否允许成员退出。允许：Allow，不允许：Denied。
+ * @method void setIsAllowQuit(string $IsAllowQuit) 设置是否允许成员退出。允许：Allow，不允许：Denied。
  * @method string getPayUin() 获取代付者Uin。成员代付费时需要
  * @method void setPayUin(string $PayUin) 设置代付者Uin。成员代付费时需要
- * @method array getIdentityRoleID() 获取成员访问身份ID列表。可以调用ListOrganizationIdentity获取，1默认支持
- * @method void setIdentityRoleID(array $IdentityRoleID) 设置成员访问身份ID列表。可以调用ListOrganizationIdentity获取，1默认支持
- * @method integer getAuthRelationId() 获取认证主体关系ID。给不同主体创建成员时需要，可以调用DescribeOrganizationAuthNode获取
- * @method void setAuthRelationId(integer $AuthRelationId) 设置认证主体关系ID。给不同主体创建成员时需要，可以调用DescribeOrganizationAuthNode获取
+ * @method string getRelationAuthName() 获取互信实名主体名称。
+ * @method void setRelationAuthName(string $RelationAuthName) 设置互信实名主体名称。
+ * @method array getAuthFile() 获取互信主体证明文件列表。
+ * @method void setAuthFile(array $AuthFile) 设置互信主体证明文件列表。
  * @method array getTags() 获取成员标签列表。最大10个
  * @method void setTags(array $Tags) 设置成员标签列表。最大10个
  */
-class CreateOrganizationMemberRequest extends AbstractModel
+class InviteOrganizationMemberRequest extends AbstractModel
 {
+    /**
+     * @var integer 被邀请账号Uin。
+     */
+    public $MemberUin;
+
     /**
      * @var string 成员名称。最大长度为25个字符，支持英文字母、数字、汉字、符号+@、&._[]-:,
      */
@@ -66,19 +71,14 @@ class CreateOrganizationMemberRequest extends AbstractModel
     public $NodeId;
 
     /**
-     * @var string 账号名称。最大长度为25个字符，支持英文字母、数字、汉字、符号+@、&._[]-:,
-     */
-    public $AccountName;
-
-    /**
      * @var string 备注。
      */
     public $Remark;
 
     /**
-     * @var integer 成员创建记录ID。创建异常重试时需要
+     * @var string 是否允许成员退出。允许：Allow，不允许：Denied。
      */
-    public $RecordId;
+    public $IsAllowQuit;
 
     /**
      * @var string 代付者Uin。成员代付费时需要
@@ -86,14 +86,14 @@ class CreateOrganizationMemberRequest extends AbstractModel
     public $PayUin;
 
     /**
-     * @var array 成员访问身份ID列表。可以调用ListOrganizationIdentity获取，1默认支持
+     * @var string 互信实名主体名称。
      */
-    public $IdentityRoleID;
+    public $RelationAuthName;
 
     /**
-     * @var integer 认证主体关系ID。给不同主体创建成员时需要，可以调用DescribeOrganizationAuthNode获取
+     * @var array 互信主体证明文件列表。
      */
-    public $AuthRelationId;
+    public $AuthFile;
 
     /**
      * @var array 成员标签列表。最大10个
@@ -101,16 +101,16 @@ class CreateOrganizationMemberRequest extends AbstractModel
     public $Tags;
 
     /**
+     * @param integer $MemberUin 被邀请账号Uin。
      * @param string $Name 成员名称。最大长度为25个字符，支持英文字母、数字、汉字、符号+@、&._[]-:,
      * @param string $PolicyType 关系策略。取值：Financial
      * @param array $PermissionIds 成员财务权限ID列表。取值：1-查看账单、2-查看余额、3-资金划拨、4-合并出账、5-开票、6-优惠继承、7-代付费，1、2 默认必须
      * @param integer $NodeId 成员所属部门的节点ID。可以通过[DescribeOrganizationNodes](https://cloud.tencent.com/document/product/850/82926)获取
-     * @param string $AccountName 账号名称。最大长度为25个字符，支持英文字母、数字、汉字、符号+@、&._[]-:,
      * @param string $Remark 备注。
-     * @param integer $RecordId 成员创建记录ID。创建异常重试时需要
+     * @param string $IsAllowQuit 是否允许成员退出。允许：Allow，不允许：Denied。
      * @param string $PayUin 代付者Uin。成员代付费时需要
-     * @param array $IdentityRoleID 成员访问身份ID列表。可以调用ListOrganizationIdentity获取，1默认支持
-     * @param integer $AuthRelationId 认证主体关系ID。给不同主体创建成员时需要，可以调用DescribeOrganizationAuthNode获取
+     * @param string $RelationAuthName 互信实名主体名称。
+     * @param array $AuthFile 互信主体证明文件列表。
      * @param array $Tags 成员标签列表。最大10个
      */
     function __construct()
@@ -126,6 +126,10 @@ class CreateOrganizationMemberRequest extends AbstractModel
         if ($param === null) {
             return;
         }
+        if (array_key_exists("MemberUin",$param) and $param["MemberUin"] !== null) {
+            $this->MemberUin = $param["MemberUin"];
+        }
+
         if (array_key_exists("Name",$param) and $param["Name"] !== null) {
             $this->Name = $param["Name"];
         }
@@ -142,28 +146,29 @@ class CreateOrganizationMemberRequest extends AbstractModel
             $this->NodeId = $param["NodeId"];
         }
 
-        if (array_key_exists("AccountName",$param) and $param["AccountName"] !== null) {
-            $this->AccountName = $param["AccountName"];
-        }
-
         if (array_key_exists("Remark",$param) and $param["Remark"] !== null) {
             $this->Remark = $param["Remark"];
         }
 
-        if (array_key_exists("RecordId",$param) and $param["RecordId"] !== null) {
-            $this->RecordId = $param["RecordId"];
+        if (array_key_exists("IsAllowQuit",$param) and $param["IsAllowQuit"] !== null) {
+            $this->IsAllowQuit = $param["IsAllowQuit"];
         }
 
         if (array_key_exists("PayUin",$param) and $param["PayUin"] !== null) {
             $this->PayUin = $param["PayUin"];
         }
 
-        if (array_key_exists("IdentityRoleID",$param) and $param["IdentityRoleID"] !== null) {
-            $this->IdentityRoleID = $param["IdentityRoleID"];
+        if (array_key_exists("RelationAuthName",$param) and $param["RelationAuthName"] !== null) {
+            $this->RelationAuthName = $param["RelationAuthName"];
         }
 
-        if (array_key_exists("AuthRelationId",$param) and $param["AuthRelationId"] !== null) {
-            $this->AuthRelationId = $param["AuthRelationId"];
+        if (array_key_exists("AuthFile",$param) and $param["AuthFile"] !== null) {
+            $this->AuthFile = [];
+            foreach ($param["AuthFile"] as $key => $value){
+                $obj = new AuthRelationFile();
+                $obj->deserialize($value);
+                array_push($this->AuthFile, $obj);
+            }
         }
 
         if (array_key_exists("Tags",$param) and $param["Tags"] !== null) {
