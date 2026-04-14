93f07b710467d61ed37caf0054b10831dcac3f28
tencentcloudapi <tencentcloudapi@tencent.com>
2026-04-15 04:25:00 +0800

    release 3.0.1569

---
+++ src/TencentCloud/Organization/V20210331/Models/UpdateIPWhitelistRequest.php
@@ -0,0 +1,65 @@
+<?php
+/*
+ * Copyright (c) 2017-2025 Tencent. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+namespace TencentCloud\Organization\V20210331\Models;
+use TencentCloud\Common\AbstractModel;
+
+/**
+ * UpdateIPWhitelist请求参数结构体
+ *
+ * @method string getZoneId() 获取空间ID
+ * @method void setZoneId(string $ZoneId) 设置空间ID
+ * @method array getIpWhitelist() 获取ip白名单（上限100个）
+ * @method void setIpWhitelist(array $IpWhitelist) 设置ip白名单（上限100个）
+ */
+class UpdateIPWhitelistRequest extends AbstractModel
+{
+    /**
+     * @var string 空间ID
+     */
+    public $ZoneId;
+
+    /**
+     * @var array ip白名单（上限100个）
+     */
+    public $IpWhitelist;
+
+    /**
+     * @param string $ZoneId 空间ID
+     * @param array $IpWhitelist ip白名单（上限100个）
+     */
+    function __construct()
+    {
+
+    }
+
+    /**
+     * For internal only. DO NOT USE IT.
+     */
+    public function deserialize($param)
+    {
+        if ($param === null) {
+            return;
+        }
+        if (array_key_exists("ZoneId",$param) and $param["ZoneId"] !== null) {
+            $this->ZoneId = $param["ZoneId"];
+        }
+
+        if (array_key_exists("IpWhitelist",$param) and $param["IpWhitelist"] !== null) {
+            $this->IpWhitelist = $param["IpWhitelist"];
+        }
+    }
+}
